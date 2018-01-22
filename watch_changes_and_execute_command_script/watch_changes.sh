5507a8b831fb23f002c5aeefc26c3b31d082060d
manu buhay <manubuhay@gmail.com>
2018-01-22 19:13:00 +0800

    added script to watch changes to a directory and trigger a command afterwards

---
+++ watch_changes_and_execute_command_script/watch_changes.sh
@@ -0,0 +1,11 @@
+for (( i=0; i<12; i++ ))
+do
+inotifywait -re create -re modify "/var/www/html/ampwebsite/html/customize/asset"
+if [ $? == 0 ]
+then
+       sleep 3
+       echo "Applying chmod command." >> /root/script/watch_changes.log 
+       chmod -R 774 /var/www/html/ampwebsite/html/customize/asset/*.*
+fi
+sleep 5
+done
