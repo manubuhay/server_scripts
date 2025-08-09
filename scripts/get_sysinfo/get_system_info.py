e816a3400fd89a0aea9ea078a216ca549d16e360
Manu Buhay <manubuhay@gmail.com>
2025-08-09 19:05:04 +0800

    added get system info script

---
+++ scripts/get_sysinfo/get_system_info.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+
+if os.name == "nt":
+    shell_value=True
+    count = "-n"
+    results = subprocess.run(["wmic", "bios", "get", "serialnumber"], shell=shell_value, capture_output=True, text=True)
+    cmd = subprocess.run(["systeminfo.exe"], shell=shell_value, capture_output=True, text=True)
+    pipe1 = subprocess.run(["findstr","Manufacturer"],shell=True, capture_output=True, text=True, input=cmd.stdout)
+    pipe2 = subprocess.run(["findstr","Model"], shell=True, capture_output=True, text=True, input=cmd.stdout)
+    owner=input("Owner: ")
+    output = "---------------------------\n %s\n %s\n\n %s\n %s\n---------------------------\n"%(results.stdout, owner, pipe1.stdout, pipe2.stdout)
+    file = open("\\\\Server_IP\\DIR1\\DIR2\\system.txt", "a")
+    file.write(output)
+    file.close()
+else:
+    print("Operating system is not Windows!")
---
+++ scripts/get_sysinfo/install.bat
@@ -0,0 +1,5 @@
+:: start /d "\\10.1.3.200\Misc\get_sysinfo"
+\\Server_IP\DIR1\DIR2\python-3.12.3-amd64.exe
+\\Server_IP\DIR1\DIR2\\get_system_info.py
+pause
+
