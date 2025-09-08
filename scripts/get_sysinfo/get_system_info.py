4133de4f3361364b41ab725144d9c5047825acb7
manu buhay <manubuhay@gmail.com>
2025-09-08 15:54:09 +0800

    added new command to get serial number since wmic is deprecated in W11

--- scripts/get_sysinfo/get_system_info.py
+++ scripts/get_sysinfo/get_system_info.py
@@ -5,6 +5,10 @@ if os.name == "nt":
     shell_value=True
     count = "-n"
     results = subprocess.run(["wmic", "bios", "get", "serialnumber"], shell=shell_value, capture_output=True, text=True)
+    #Source: https://superuser.com/questions/1178674/wmic-is-not-recognized-as-an-internal-or-external-command-operable-program-or
+    # wmic is deprecated in W11, use "Get-WmiObject -Class Win32_Bios | Select SerialNumber" instead
+    wmi = subprocess.run(["Get-WmiObject", "-Class", "Win32_Bios"], shell=shell_value, capture_output=True, text=True)
+    wmi_pipe = subprocess.run(["Select","SerialNumber"],shell=True, capture_output=True, text=True, input=wmi.stdout)
     cmd = subprocess.run(["systeminfo.exe"], shell=shell_value, capture_output=True, text=True)
     pipe1 = subprocess.run(["findstr","Manufacturer"],shell=True, capture_output=True, text=True, input=cmd.stdout)
     pipe2 = subprocess.run(["findstr","Model"], shell=True, capture_output=True, text=True, input=cmd.stdout)
