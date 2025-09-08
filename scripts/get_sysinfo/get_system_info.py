ff247f8adf9f41291670d3f53441131fe9201050
manu buhay <manubuhay@gmail.com>
2025-09-08 16:08:52 +0800

    added powershell parameter to wmi to make subprocess run in powershell insead of command prompt

--- scripts/get_sysinfo/get_system_info.py
+++ scripts/get_sysinfo/get_system_info.py
@@ -7,7 +7,7 @@ if os.name == "nt":
     results = subprocess.run(["wmic", "bios", "get", "serialnumber"], shell=shell_value, capture_output=True, text=True)
     #Source: https://superuser.com/questions/1178674/wmic-is-not-recognized-as-an-internal-or-external-command-operable-program-or
     # wmic is deprecated in W11, use "Get-WmiObject -Class Win32_Bios | Select SerialNumber" instead
-    wmi = subprocess.run(["Get-WmiObject", "-Class", "Win32_Bios"], shell=shell_value, capture_output=True, text=True)
+    wmi = subprocess.run(["powershell","Get-WmiObject", "-Class", "Win32_Bios"], shell=shell_value, capture_output=True, text=True)
     wmi_pipe = subprocess.run(["Select","SerialNumber"],shell=True, capture_output=True, text=True, input=wmi.stdout)
     cmd = subprocess.run(["systeminfo.exe"], shell=shell_value, capture_output=True, text=True)
     pipe1 = subprocess.run(["findstr","Manufacturer"],shell=True, capture_output=True, text=True, input=cmd.stdout)
